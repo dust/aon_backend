d15173b9cdeb2a187ad886b89abc0daba588c1cb
Dust <dust@live.com>
2025-01-10 18:01:58 +0800

    uwsgi

--- aon/wsgi.py
+++ aon/wsgi.py
@@ -1,12 +1,12 @@
 from aon.factory import create_app
-from flask import request
+# from flask import request
 from flask_cors import CORS
-from vercel.wsgi import VercelWSGI
+# from vercel.wsgi import VercelWSGI
 
 app = create_app(config_name="DEVELOPMENT")
 CORS(app, supports_credentials=True)
 
-app_wsgi = VercelWSGI(app)
+# app_wsgi = VercelWSGI(app)
 
-def main(request):
-    return app_wsgi(request)
+# def main(request):
+    # return app_wsgi(request)
