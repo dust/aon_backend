f9a17b5e6db91d22ce6a30d67920d71b8d026c02
Dust <dust@live.com>
2025-01-22 21:12:48 +0800

    cors

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,10 @@
 from aon.factory import create_app
 # from flask import request
-# from flask_cors import CORS
+from flask_cors import CORS
 # from vercel.wsgi import VercelWSGI
 
 app = create_app(config_name="DEVELOPMENT")
-# CORS(app, supports_credentials=True)
+CORS(app, resources={r"/*": {"origins": "https://launch.agiopen.network", "supports_credentials": True}})
 
 # app_wsgi = VercelWSGI(app)
 
